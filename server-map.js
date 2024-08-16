22be623289253aa4ddd567783c380299fd66a305
DelmMV <opi588@yandex.ru>
2024-08-16 18:10:41 +0300

    init

--- server-map.js
+++ server-map.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
-const cors = require('cors');
 const haversine = require('haversine-distance');
 
 const app = express();
@@ -25,25 +24,6 @@ async function connectToMongoDB() {
 		process.exit(1); // Завершаем процесс, если подключение не удалось
 	}
 }
-
-const corsOptions = {
-	origin: 'http://188.243.88.61', // Разрешить запросы с этого домена
-	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Разрешить все эти методы
-	credentials: true, // Если вам нужно передавать куки или авторизационные данные
-	optionsSuccessStatus: 200 // Некоторые старые браузеры могут не поддерживать статус 204 для успешных запросов
-};
-
-app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'http://188.243.88.61');
-	res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-	next();
-});
-
-app.options('*', cors(corsOptions)); // Обработка предзапросов
-
 
 app.get('/api/route/:userId', async (req, res) => {
 	const { userId } = req.params;
