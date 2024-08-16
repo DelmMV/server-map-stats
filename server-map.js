d6d1eebeb3f05d6d2e8075c8f51cf86791765aed
DelmMV <opi588@yandex.ru>
2024-08-16 18:07:16 +0300

    init

--- server-map.js
+++ server-map.js
@@ -35,6 +35,15 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', 'http://188.243.88.61');
+	res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	next();
+});
+
+app.options('*', cors(corsOptions)); // Обработка предзапросов
+
 
 app.get('/api/route/:userId', async (req, res) => {
 	const { userId } = req.params;
